47a665822f690d406bcdf96a07a3807f4ff9606f
Ananya Chakraborty Gourab <aborty174@gmail.com>
2026-04-28 11:57:22 +0600

    better auth added and mongo url added

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import Navbar from '@/components/shared/Navbar'
 import { Montserrat } from 'next/font/google'
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
+const client = new MongoClient("mongodb://localhost:27017/database");
+const db = client.db();
 
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client
+  }),
+});
 const montserrat = Montserrat({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
